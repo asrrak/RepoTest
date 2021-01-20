2daa8ad1c3e38810df15b585cddcf642c155eed6
arcast <heliotrop.soul@gmail.com>
2021-01-19 20:28:59 -0600

    FirstEverCommit

---
+++ src/com/arcast/Main.java
@@ -0,0 +1,10 @@
+package com.arcast;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String pijama = "topo";
+        System.out.print(pijama);
+    }
+}
