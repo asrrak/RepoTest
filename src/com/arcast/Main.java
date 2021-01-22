92922e76d2a438ee7eaee7233d2057219ca02b67
Amar Asrrak <heliotrop.soul@gmail.com>
2021-01-21 20:57:22 -0600

    CommitTest3

--- src/com/arcast/Main.java
+++ src/com/arcast/Main.java
@@ -3,7 +3,7 @@ package com.arcast;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here Commit Test 2
+	// write your code here Commit Test 3
         String pijama = "topo";
         System.out.print(pijama);
     }
